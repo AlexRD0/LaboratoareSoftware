5915ff8783d50bac64adaaf5079413f7c22bbe5c
al-ex3 <dumitrualexandruradu@gmail.com>
2026-04-21 09:12:37 +0300

    laborator 7

---
+++ src/main/java/lab7/util/PasswordMaker.java
@@ -0,0 +1,42 @@
+package lab7.util;
+
+public class PasswordMaker {
+    private static final int MAGIC_NUMBER = 3;
+    private static final String MAGIC_STRING;
+
+    private String name;
+
+    private static PasswordMaker instance = null;
+
+    private static int counter;
+
+    static {
+        StringRandomizer srand = new StringRandomizer();
+        MAGIC_STRING = srand.randomString(20);
+    }
+
+    private PasswordMaker(String name) {
+        this.name = name;
+    }
+
+    public static PasswordMaker getInstance(){
+        if(instance == null){
+            instance = new PasswordMaker("Alex");
+        }
+        counter++;
+        return instance;
+    }
+
+    public static int getCallingCounts(){
+        return counter;
+    }
+
+    public String getPassword() {
+        java.util.Random r = new java.util.Random();
+        String ln = "" + name.length();
+        ln += r.nextInt(101);
+        StringRandomizer srand = new StringRandomizer();
+        return srand.randomString(MAGIC_NUMBER) + srand.randomString(10, MAGIC_STRING) + ln;
+    }
+}
+
